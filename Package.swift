1677f22aeecfd39675bf038accd549e3d4b009dc
Stephen Beitzel <sbeitzel@pobox.com>
2021-09-15 10:19:36 -0700

    Update Swift tools to allow specifying macOS target

--- Package.swift
+++ Package.swift
@@ -1,10 +1,13 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "PassEncoder",
+    platforms: [
+       .macOS(.v10_11)
+    ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
