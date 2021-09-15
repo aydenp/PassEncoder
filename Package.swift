6c55e0466831b0b994ca023544ee25835a006452
Stephen Beitzel <sbeitzel@pobox.com>
2021-09-15 10:25:02 -0700

    Set minimum macOS version to 10.12

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "PassEncoder",
     platforms: [
-       .macOS(.v10_11)
+       .macOS(.v10_12)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
