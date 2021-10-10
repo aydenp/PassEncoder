4feba84d20568246a2a2910221687c24ef6c11dc
Ayden Panhuyzen <git@ayden.dev>
2021-10-10 17:44:57 -0400

    Disable failing placeholder test

--- Tests/PassEncoderTests/PassEncoderTests.swift
+++ Tests/PassEncoderTests/PassEncoderTests.swift
@@ -6,7 +6,7 @@ final class PassEncoderTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(PassEncoder().text, "Hello, World!")
+//        XCTAssertEqual(PassEncoder().text, "Hello, World!")
     }
 
 
