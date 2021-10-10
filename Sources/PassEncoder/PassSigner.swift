2c3f1f22a92da53c85c8beb4a1b46725d40872a0
Ayden Panhuyzen <git@ayden.dev>
2021-10-10 17:44:46 -0400

    Make PassSigner a class

--- Sources/PassEncoder/PassSigner.swift
+++ Sources/PassEncoder/PassSigner.swift
@@ -10,20 +10,20 @@
 import Foundation
 
 /// A singleton class used to sign PassKit passes.
-public struct PassSigner {
+public class PassSigner {
     /// The shared instance of `PassSigner`.
-    public static var shared = PassSigner()
-    
+    public static let shared = PassSigner()
+
     // MARK: - Options
     /// The local URL of the Apple Worldwide Developer Relations Root Certificate Authority (required to sign passes).
     public var appleWWDRCertURL: URL?
     /// The URL to your OpenSSL binary. Set it if yours is in a different location than /usr/bin/openssl.
     public var openSSLURL = URL(fileURLWithPath: "/usr/bin/openssl")
-    
+
     private init() {}
-    
+
     // MARK: - Signing
-    
+
     /**
      Sign the provided pass manifest data.
      - Note: This method will fail and crash your program if you haven't set `appleWWDRCertURL`.
@@ -34,7 +34,7 @@ public struct PassSigner {
      */
     public func signPassManifest(at url: URL, toSignatureAt signatureURL: URL, info: SigningInfo) -> Bool {
         guard let caURL = appleWWDRCertURL else { fatalError("You must specify the location of your Apple WWDR CA certificate (PassSigner.shared.appleWWDRCertURL).") }
-        
+
         let task = Process()
         task.launchPath = openSSLURL.path
         task.arguments = ["smime", "-sign", "-signer", info.certificate.path, "-inkey", info.certificate.path, "-certfile", caURL.path, "-in", url.path, "-out", signatureURL.path, "-outform", "der", "-binary", "-passin", "pass:\(info.password)"]
